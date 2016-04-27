b205e063f86c37fe1ffe64cddd523ed4649f82e6
Fernando Borretti <eudoxiahp@gmail.com>
2016-04-26 21:24:43 -0300

    Don't really need this anymore

--- server.js
+++ server.js
@@ -4,17 +4,11 @@ var app = express();
 var sync = require('synchronize');
 var cors = require('cors');
 
-// Use fibers in all routes so we can use sync.await() to make async code easier to work with.
-app.use(function(req, res, next) {
-  sync.fiber(next);
-});
-
 // Since Mixmax calls this API directly from the client-side, it must be whitelisted.
 var corsOptions = {
   origin: /^[^.\s]+\.mixmax\.com$/,
   credentials: true
 };
-
 
 app.get('/typeahead', cors(corsOptions), require('./api/typeahead'));
 app.get('/resolver', cors(corsOptions), require('./api/resolver'));
